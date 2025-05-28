1255b8e24876d0a0db71a41f8d6b2e9a2316752a
joaovtrsantos <dev.joaovitor1@gmail.com>
2025-05-28 08:50:16 -0300

    feat: add intl lib and format date.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -79,7 +80,7 @@ class MyHomePage extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(tr.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                        Text(tr.date.toString(), style: TextStyle(color: Colors.grey),)
+                        Text(DateFormat('d MMM y').format(tr.date), style: TextStyle(color: Colors.grey),)
                       ],
                     )
                   ],
